2f75d95d29904e285f651af856849fc7842331da
Gonçalo <goncaloscravo@gmail.com>
2024-03-13 12:20:44 +0000

    add: quiz structure

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,45 @@
+import { useCallback, useState } from "react";
+import QUESTIONS from "../questions.js";
+import image from "../assets/quiz-complete.png";
+import Question from "./Question.jsx";
+
+export default function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
+    setUserAnswers((prevAnswers) => {
+      return [...prevAnswers, selectedAnswer];
+    });
+  },
+  []);
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
+
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={image} alt="quiz-complete" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  return (
+    <div id="quiz">
+      <Question
+        key={activeQuestionIndex}
+        questionIndex={activeQuestionIndex}
+        onSelectAnswer={handleSelectAnswer}
+        onSkipAnswer={handleSkipAnswer}
+      />
+    </div>
+  );
+}
